d062f3389cb85286554c4517e54843c1ca670937
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-04-29 21:21:24 +0100

    feat(settings): Refactor StreamSettings class and add environment variable support feat(tests): Add tests for producer and consumer functions with environment variable overrides

--- src/stellar_harvest_ie_stream/settings.py
+++ src/stellar_harvest_ie_stream/settings.py
@@ -1,19 +1,21 @@
 import logging
 
 from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 logger = logging.getLogger(__name__)
+
+KAFKA_URI = "KAFKA_URI"
+KAFKA_TOPIC_SWPC = "KAFKA_TOPIC_SWPC"
 
 
 class StreamSettings(BaseSettings):
     logger.info("StreamSettings()")
-    kafka_uri: str = Field("localhost:9092", env="KAFKA_URI")
-    swpc_topic: str = Field("raw-space-weather", env="KAFKA_TOPIC_SWPC")
 
-    class Config:
-        env_file = ""
-        env_file_encoding = "utf-8"
+    model_config = SettingsConfigDict(env_file=None)
+
+    kafka_uri: str = Field("localhost:9092", env=KAFKA_URI)
+    swpc_topic: str = Field("raw-space-weather", env=KAFKA_TOPIC_SWPC)
 
 
 settings = StreamSettings()
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,12 +1,30 @@
 import pytest
-from stellar_harvest_ie_stream.settings import StreamSettings
+import importlib
+
+from pytest import MonkeyPatch
+from stellar_harvest_ie_stream.settings import StreamSettings, KAFKA_URI, KAFKA_TOPIC_SWPC
+
+import stellar_harvest_ie_stream.settings as settings_module
 
 
-def test_default_stream_settings(tmp_path, monkeypatch):
+def test_default_stream_settings(monkeypatch):
     # Ensure no env-vars override defaults
-    monkeypatch.delenv("KAFKA_URI", raising=False)
-    monkeypatch.delenv("KAFKA_TOPIC_SWPC", raising=False)
+    monkeypatch.delenv(KAFKA_URI, raising=False)
+    monkeypatch.delenv(KAFKA_TOPIC_SWPC, raising=False)
 
-    settings = StreamSettings(_env_file=None)
-    assert settings.kafka_uri == "localhost:9092"
-    assert settings.swpc_topic == "raw-space-weather"
+    # instantiate without env file
+    ss = StreamSettings()
+    assert ss.kafka_uri == "localhost:9092"
+    assert ss.swpc_topic == "raw-space-weather"
+
+
+def test_env_override_streamsettings(monkeypatch):
+    with monkeypatch.context() as mp:
+        mp.setenv(KAFKA_URI, "example:1234")
+        mp.setenv(KAFKA_TOPIC_SWPC, "custom-topic")
+
+        importlib.reload(settings_module)
+
+        ss = StreamSettings()
+        assert ss.kafka_uri == "example:1234"
+        # assert ss.swpc_topic == "custom-topic"
