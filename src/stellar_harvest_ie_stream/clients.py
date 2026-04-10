087fbdc1c6965f692432f97c544a63d2d6138fe8
Machine Learning Maverick <github@machinelearningmaverick.com>
2026-04-10 13:45:36 +0100

    refactor(singleton): add module-level variable to act like a singleton

--- src/stellar_harvest_ie_stream/clients.py
+++ src/stellar_harvest_ie_stream/clients.py
@@ -5,12 +5,23 @@ from kafka import KafkaProducer, KafkaConsumer
 from stellar_harvest_ie_stream.settings import settings
 
 
+_producer: KafkaProducer | None = None
+
+
 @log_io()
 def get_producer() -> KafkaProducer:
-    return KafkaProducer(
-        bootstrap_servers=[settings.kafka_uri],
-        value_serializer=lambda v: json.dumps(v, default=str).encode(),
-    )
+    global _producer
+    if _producer is None:
+        _producer = KafkaProducer(
+            bootstrap_servers=[settings.kafka_uri],
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            acks="all",
+            retries=5,
+            retry_backoff_ms=300,
+            linger_ms=10,
+            # compression_type="gzip",
+        )
+    return _producer
 
 
 @log_io()
