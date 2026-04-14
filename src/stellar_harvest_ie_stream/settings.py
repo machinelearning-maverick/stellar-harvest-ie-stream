94ebe18562a1854a2614db885705d88593a789f1
Machine Learning Maverick <github@machinelearningmaverick.com>
2026-04-14 19:18:23 +0100

    fix(logging): initialize setup_logger() in the proper way, once per module main entry point

--- src/stellar_harvest_ie_stream/settings.py
+++ src/stellar_harvest_ie_stream/settings.py
@@ -1,10 +1,8 @@
 import logging
 
 from pydantic import Field
-from stellar_harvest_ie_config.logging_config import setup_logging
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-setup_logging()
 logger = logging.getLogger(__name__)
 
 KAFKA_URI = "KAFKA_URI"
@@ -12,12 +10,16 @@ KAFKA_TOPIC_SWPC = "KAFKA_TOPIC_SWPC"
 
 
 class StreamSettings(BaseSettings):
-    logger.info("StreamSettings()")
-
     model_config = SettingsConfigDict(env_file=None)
 
     kafka_uri: str = Field("kafka:9092", env=KAFKA_URI)
-    swpc_topic: str = Field("stellar-harvest-ie-raw-space-weather", env=KAFKA_TOPIC_SWPC)
+    swpc_topic: str = Field(
+        "stellar-harvest-ie-raw-space-weather", env=KAFKA_TOPIC_SWPC
+    )
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        logger.info("StreamSettings()")
 
 
 settings = StreamSettings()
